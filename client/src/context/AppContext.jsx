6ba83d85d970bfc48f62fbb5da7c98212ffd25aa
ratnadeepa-01 <ratnadeepakesavan@gmail.com>
2026-04-05 22:46:32 +0530

    feat: integrate react-hot-toast for notifications; enhance AppContext with user authentication and car fetching logic

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,13 +1,82 @@
 import { useContext } from "react";
 import { createContext } from "react";
+import axios from 'axios';
+import {toast} from 'react-hot-toast'
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { useEffect } from "react";
 
 
+axios.defaults.baseURL = import.meta.env.VITE_BASE_URL
 export const AppContext = createContext();
 
 export const AppProvider = ({children})=>{
   
-    const value = {
+    const navigate = useNavigate()
+    const currency = import.meta.env.VITE_CURRENCY
+    const [token, setToken] = useState(null)
+    const [user, setUser] = useState(null)
+    const [isOwner, setIsOwner] = useState(false)
+    const [pickupDate, setPickupDate] = useState('')
+    const [returnDate, setReturnDate] = useState('')
 
+    const [cars, setCars] = useState([])
+
+    //Function to check is user is logged in
+    const fetchUser = async()=>{
+        try {
+           const {data} = await axios.get('/api/user/data')
+           if(data.success){
+            setUser(data.user)
+            setIsOwner(data.user.role === 'owner')
+           }else{
+            navigate('/ ')
+           }
+        } catch (error) {
+            toast.error(error.message);
+        }
+    }
+
+    //function to log out the user
+    const logout =()=>{
+        localStorage.removeItem('token')
+        setToken(null)
+        setUser(null)
+        setIsOwner(fasle)
+        axios.defaults.headers.common['Authorization'] = ''
+        toast.success('You have been logged out')
+    }
+
+    //useeffect to retrive the token from locacl storage
+    useEffect(()=>{
+        const token = localStorage.getItem('token')
+        setToken(token)
+        fetchCars()
+    },[])
+
+
+    //function to fetch all cars from the server
+    const fetchCars = async()=>{
+        try {
+            const {data} = await axios.get('/api/user/cars')
+            data.success ? setCars(data.cars) : toast.error(data.message)
+        } catch (error) {
+            toast.error(error.message)
+        }
+    }
+
+
+    //useeffect to fetch user data when token is available
+    useEffect(()=>{
+        if(token){
+            axios.defaults.headers.common['Authorization'] = `${token}`
+            fetchUser()
+        }
+    },[token])
+
+
+    const value = {
+        navigate,currency
     }
 
     return
